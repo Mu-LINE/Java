1a61c74eb9a737e49fb136027e295991fe34d28d
Mu-LINE <48792291+Mu-LINE@users.noreply.github.com>
2019-08-22 13:17:31 +0900

    Create JankenData.java
    Gu, Choki, Pa

---
+++ JankenData.java
@@ -0,0 +1,14 @@
+
+public class JankenData {
+	// じゃんけんの乱数処理
+	private String[] janken = new String[3];
+	
+	public void setJanken(String[] janken) {
+		this.janken = janken;
+	}
+	
+	public String[] getJanken() {
+		return janken;
+	}
+	
+}
